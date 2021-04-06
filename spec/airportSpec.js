3bbe99bab5525ba393048ba5a29a7420668db624
Louise Ballantyne <lousballantyne@icloud.com>
2021-04-06 17:32:33 +0100

    Added prevention of landing when weather is stormy

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,10 +1,27 @@
 describe('Airport', function() {
+
   describe('land', function() {
     it('instructs a plane to land', function() {
       var airport = new Airport();
       var plane = new Plane();
       airport.Land(plane);
       expect(airport.apron.includes(plane)).toBe(true)
+    });
+
+  describe('weather', function() {
+    it('cannot land if the weather is stormy', function() {
+      var airport = new Airport();
+      var plane = jasmine.createSpyObj('plane',['isStormy']);
+      plane.isStormy(true);
+      expect(function() { airport.Land(plane); } ).toThrow(new Error("Plane cannot land in stormy weather"))
+    });
+  });
+
+    it('prevents landing when the airport is full', function() {
+      var airport = new Airport(1);
+      var plane = new Plane();
+      airport.Land(plane);
+      expect(function() { airport.Land(plane); } ).toThrow(new Error("Airport has reached capacity"))
     });
   });
 
@@ -16,6 +33,5 @@ describe('Airport', function() {
       airport.TakeOff(plane)
       expect(airport.apron.includes(plane)).toBe(false)
     });
-
   });
 });
--- src/airport.js
+++ src/airport.js
@@ -1,10 +1,21 @@
 class Airport {
-  constructor(){
+  constructor( number=20 ){
+    this.capacity = number;
     this.apron = [];
   }
   
   Land(plane) {
-    this.apron.push(plane);
+    if (plane.isStormy) {
+      var error = new Error ( "Plane cannot land in stormy weather" );
+      throw error ;
+    }
+    else if (this.apron.length >= this.capacity) {
+      var error = new Error ( "Airport has reached capacity" );
+      throw error ;
+    }
+    else {
+      this.apron.push(plane);
+    }
   }
 
   TakeOff(plane) {
